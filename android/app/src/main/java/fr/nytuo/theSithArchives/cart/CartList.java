f992f0d065583f54427b48fe4b88c0c234479a29
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2023-04-04 08:33:58 +0200

    finition de Cart

--- android/app/src/main/java/fr/nytuo/theSithArchives/cart/CartList.java
+++ android/app/src/main/java/fr/nytuo/theSithArchives/cart/CartList.java
@@ -4,18 +4,33 @@ import java.util.ArrayList;
 
 import fr.nytuo.theSithArchives.productsList.Product;
 
+/**
+ * Liste des produits du panier
+ */
 public class CartList extends ArrayList<Product> {
+    /**
+     * Instance de la liste
+     */
     private static CartList instance;
 
+    /**
+     * Retourne l'instance de la liste
+     * @return L'instance de la liste
+     */
     public static CartList getInstance(){
         if (instance==null) {
             instance = new CartList();
         }
         return instance;
     }
+
     public CartList() {
     }
 
+    /**
+     * Retourne le prix total de la liste
+     * @return Le prix total de la liste
+     */
     public int getTotalPrice() {
         int totalPrice = 0;
         for (int i = 0; i < this.size(); i++) {
